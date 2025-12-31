eb76a53d8b368c20a16bbe51a5b13c57fc37b226
Stephen McGurrin <stibh@Stephens-Mac-mini.local>
2025-12-31 04:00:25 +0000

    nvim transparent

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -877,27 +877,34 @@ require('lazy').setup({
 	},
 
 	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		--	 Change the name of the colorscheme plugin below, and then
+		--	 change the command in the config to whatever the name of that colorscheme is.
+
+		--	 If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		'folke/tokyonight.nvim',
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		config = function()
 			---@diagnostic disable-next-line: missing-fields
-			require('rose-pine').setup {
+			require('tokyonight').setup {
+				transparent = true,
 				styles = {
 					comments = { italic = false }, -- Disable italics in comments
+					sidebars = 'transparent',
+					floats = 'transparent',
 				},
 			}
 
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme 'rose-pine'
+			vim.cmd 'colorscheme tokyonight-moon'
 		end,
 	},
 
+	-- vim.cmd("colorscheme rose-pine")
+	-- vim.cmd("colorscheme rose-pine-main")
+	-- vim.cmd("colorscheme rose-pine-moon")
+	-- vim.cmd("colorscheme rose-pine-dawn")
 	--  vim.cmd("colorscheme rose-pine")
 	-- vim.cmd("colorscheme rose-pine-main")
 	-- vim.cmd("colorscheme rose-pine-moon")
@@ -1015,6 +1022,13 @@ require('lazy').setup({
 		},
 	},
 })
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
